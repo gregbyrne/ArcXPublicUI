24b9f8c8d8ea2fd75e92c043779046fcccb01319
jpowell <powell.joshua@epa.gov>
2020-10-19 10:06:08 -0400

    Adding Vue Router and component routes.

--- src/main.js
+++ src/main.js
@@ -1,8 +1,25 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import App from './App.vue'
+import Home from './components/Home'
+import SearchResults from './components/SearchResults'
 
 Vue.config.productionTip = false
 
+const routes = [
+  { path: '/', name: 'home', component: Home },
+  { path: '/searchresults', name: 'searchresults', component: SearchResults }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  routes
+})
+
+// use router
+Vue.use(VueRouter);
+
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
