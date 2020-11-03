03b1b1caf8dd49b36e7375637cb1d5e9a6b27eb1
greg19735 <greg19735@gmail.com>
2020-11-03 15:17:22 -0500

    add contactus.vue to git.
    remove broken style sheets

---
+++ src/components/layout/ContactUs.vue
@@ -0,0 +1,41 @@
+<template>
+
+   <div class="region-preface clearfix">  <div id="block-pane-epa-web-area-connect" class="block block-pane  block-pane-epa-web-area-connect">
+
+
+
+      <span class="connect-title"><a href="/arc-x/forms/contact-us-about-climate-change-adaptation-resource-center" class="connect-title__link">Contact Us</a></span>
+   </div>
+      <div id="block-pane-social-sharing" class="block block-pane  block-pane-social-sharing">
+
+
+
+         <span class="connect-title">Share</span>
+         <ul class="share-links">
+            <li class="share-links__item facebook">
+               <a class="share-links__link" href="https://www.facebook.com/sharer.php?u=https://www.epa.gov/arc-x/your-climate-adaptation-search&amp;t=Your%20Climate%20Adaptation%20Search" aria-label="Share this page on Facebook" target="_blank" rel="noopener noreferrer" title="Share this page on Facebook">
+                  <svg class="icon icon--social-link" aria-hidden="true">
+                     <use xlink:href="/sites/all/themes/epa/img/svg/small-icons.svg#small-facebook"></use>
+                  </svg>
+               </a>
+            </li>
+            <li class="share-links__item twitter">
+               <a class="share-links__link" href="https://twitter.com/intent/tweet?original_referer=https://www.epa.gov/arc-x/your-climate-adaptation-search&amp;text=Your%20Climate%20Adaptation%20Search&amp;url=https://www.epa.gov/arc-x/your-climate-adaptation-search&amp;via=EPA&amp;count=none&amp;lang=en" aria-label="Tweet this page on Twitter" target="_blank" rel="noopener noreferrer" title="Tweet this page on Twitter">
+                  <svg class="icon icon--social-link" aria-hidden="true">
+                     <use xlink:href="/sites/all/themes/epa/img/svg/small-icons.svg#small-twitter"></use>
+                  </svg>
+               </a>
+            </li>
+            <li class="share-links__item email">
+               <a class="share-links__link" href="mailto:%20?subject=Shared%20from%20the%20US%20EPA:%20Your%20Climate%20Adaptation%20Search&amp;body=Your%20Climate%20Adaptation%20Search%20-%20https://www.epa.gov/arc-x/your-climate-adaptation-search" aria-label="Email this page" title="Email this page">
+                  <svg class="icon icon--social-link" aria-hidden="true">
+                     <use xlink:href="/sites/all/themes/epa/img/svg/small-icons.svg#small-email"></use>
+                  </svg>
+               </a>
+            </li>
+         </ul>
+      </div>
+   </div>
+
+
+</template>
