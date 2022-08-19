c97e98b9b5f73079573651fe6a16a645d4124818
gregbyrne <byrne.gregory@epa.gov>
2022-08-19 15:06:33 -0400

    Update home page to include link to arc-x

--- src/components/home/Home.vue
+++ src/components/home/Home.vue
@@ -2,9 +2,9 @@
 
 
   <div id="main-content" class="main-content clearfix" role="main" tabindex ="-1">
-    <h2 class = "microsite-name">Climate Change Adaptation Resource Center (ARC-X) </h2>
+    <div style = "padding-top:5px"></div>
     <app-contact></app-contact>
-
+    <div class = "web-area-title" style="margin-top:-25px; margin-bottom: 10px"> <a href="https://www.epa.gov/arc-x" >Climate Change Adaptation Resource Center (ARC-X)</a></div>
 
 
 
@@ -39,61 +39,61 @@
 
 
 
-    <app-webfooter></app-webfooter>
+      <app-webfooter></app-webfooter>
     </div>
 
   </div>
 </template>
 
 <script>
-  import aoi from './AreaOfInterest.vue'
-  import geo from './GeographicRegion.vue'
-  import contact from '../layout/ContactUs.vue'
-  import webfooter from '../layout/WebFooter.vue'
+import aoi from './AreaOfInterest.vue'
+import geo from './GeographicRegion.vue'
+import contact from '../layout/ContactUs.vue'
+import webfooter from '../layout/WebFooter.vue'
 
 
-  export default {
-    name: 'search',
-    props: ['region', 'checkedItems', 'checkedSubItems'],
-    data: function() {
-      return {
-        itemSelectionsProp: null,
-        subitemSelectionsProp: null,
-        regionProp: null,
+export default {
+  name: 'search',
+  props: ['region', 'checkedItems', 'checkedSubItems'],
+  data: function() {
+    return {
+      itemSelectionsProp: null,
+      subitemSelectionsProp: null,
+      regionProp: null,
 
-      }
-    },
-    components: {
-      'app-aoi' : aoi,
-      'app-geo' : geo,
-      'app-contact' : contact,
-      'app-webfooter' : webfooter
-    },
-    methods : {
-      checkPropValues()
-      {
-        if (this.$props.checkedItems != undefined)
-        {
-          this.itemSelectionsProp = JSON.parse(this.$props.checkedItems)
-        }
-
-        if (this.$props.checkedSubItems != undefined)
-        {
-          this.subitemSelectionsProp = JSON.parse(this.$props.checkedSubItems)
-        }
-
-        if (this.$props.region != undefined)
-        {
-          this.regionProp = this.$props.region
-        }
-      }
-    },
-    created()
-    {
-      this.checkPropValues()
     }
+  },
+  components: {
+    'app-aoi' : aoi,
+    'app-geo' : geo,
+    'app-contact' : contact,
+    'app-webfooter' : webfooter
+  },
+  methods : {
+    checkPropValues()
+    {
+      if (this.$props.checkedItems != undefined)
+      {
+        this.itemSelectionsProp = JSON.parse(this.$props.checkedItems)
+      }
 
+      if (this.$props.checkedSubItems != undefined)
+      {
+        this.subitemSelectionsProp = JSON.parse(this.$props.checkedSubItems)
+      }
+
+      if (this.$props.region != undefined)
+      {
+        this.regionProp = this.$props.region
+      }
+    }
+  },
+  created()
+  {
+    this.checkPropValues()
   }
+
+}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
