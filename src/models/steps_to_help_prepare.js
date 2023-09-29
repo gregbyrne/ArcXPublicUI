481ec45d358b31e47ea59770260e2d6066c8d221
gregbyrne <byrne.gregory@epa.gov>
2023-09-29 13:55:02 -0400

    delete everything

--- src/models/steps_to_help_prepare.js
+++
@@ -1,8 +0,0 @@
-export default class steps_to_help_prepare {
-  constructor(id, name, sub_title, description) {
-    this.id = id;
-    this.name = name;
-    this.sub_title = sub_title;
-    this.description = description;
-  }
-}
