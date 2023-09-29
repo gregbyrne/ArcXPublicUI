00cbc102e9b7336f3f08ca9007d7f4242c899e9d
gregbyrne <byrne.gregory@epa.gov>
2023-09-29 13:47:21 -0400

    new app

---
+++ src/router/index.js
@@ -0,0 +1,34 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import SearchResultsView from '../components/searchresults/SearchResults.vue'
+
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView,
+      props: true,
+
+    },
+    {
+      path: '/about',
+      name: 'about',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/AboutView.vue')
+    },
+    {
+      path: '/searchResults',
+      name: 'searchresults',
+      component: SearchResultsView,
+      props: true
+
+    }
+  ]
+})
+
+export default router
