d190720243ecfb328bf9a2ad546da37158deb6e3
gregbyrne <byrne.gregory@epa.gov>
2023-10-17 14:32:02 -0400

    change path

--- src/main.js
+++ src/main.js
@@ -14,7 +14,7 @@ import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
-import {useAllStore} from "../stores/AllStore";
+import {useAllStore} from "/stores/AllStore";
 
 const vuetify = createVuetify({
     components,
