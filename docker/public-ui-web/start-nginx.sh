6539097113c05b28b773b87e73e0be8196f061d0
Aaron Johnson <johnson.aaron@epa.gov>
2023-11-03 15:20:47 +0000

    Force overwrite.

--- docker/public-ui-web/start-nginx.sh
+++ docker/public-ui-web/start-nginx.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-envsubst < /usr/share/nginx/html/config.json.tpl > /usr/share/nginx/html/config.json;
+envsubst < /usr/share/nginx/html/config.json.tpl >! /usr/share/nginx/html/config.json;
 
 nginx -g 'daemon off;'
