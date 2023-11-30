69f96fcd8183e50db850f5ebb0ce4fa26cdfe820
Gregory Byrne <byrne.gregory@epa.gov>
2023-11-30 11:31:22 -0500

    env testing

--- src/utils/env.js
+++ src/utils/env.js
@@ -4,6 +4,7 @@ export default function getEnv() {
     if(window.location.origin.includes("localhost") ){
         environment =  "http://localhost:7101"
     }else if(window.location.origin.includes("arcx-public-ui.devsecops-dev") ){
+        console.log("staging")
         environment = "https://arc-x-public-api.devsecops-dev.aws.epa.gov"
     }else {
         environment = "https://climateadaptation.epa.gov"
